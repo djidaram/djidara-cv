908a9ba853881302dda59ca646d54b76e2c292a1
Marko Djidara <marko.djidara@hotmail.com>
2024-07-14 07:30:25 +0200

    feat: preps for smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import Contact from "@/components/chapters/Contact";
+import Resume from "@/components/chapters/Resume";
+import Work from "@/components/chapters/Work";
+import { useState } from "react";
+
 export default function Home() {
-  return <main>Homepage</main>;
+  return (
+    <main>
+      <div className="h-[1000px] bg-red-300"></div>
+
+      <Resume />
+      <div className="h-[1000px] bg-green-300"></div>
+
+      <Work />
+      <div className="h-[1000px] bg-blue-300"></div>
+      <Contact />
+    </main>
+  );
 }
---
+++ components/chapters/Work.tsx
@@ -0,0 +1,5 @@
+const Work = () => {
+  return <div id="work">Work</div>;
+};
+
+export default Work;
