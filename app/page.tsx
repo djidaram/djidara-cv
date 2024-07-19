fefa46b7b7838f6aa58cb32695d42bf4e3b4f878
Marko Djidara <marko.djidara@hotmail.com>
2024-07-19 22:35:56 +0200

    fix: removed unused dep

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Contact from "@/components/chapters/Contact";
 import Landing from "@/components/chapters/Landing";
 import Resume from "@/components/chapters/Resume";
 import Work from "@/components/chapters/Work";
-import { useState } from "react";
 
 export default function Home() {
   return (
