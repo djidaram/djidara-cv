33d56ae4ad23ee782020044264556e3e1137accd
mdjidara <m.djidara@pan-net.eu>
2024-07-21 21:56:32 +0200

    style(header): added glass effect

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="py-8 fixed w-full z-10 bg-secondary">
+    <header className="py-8 fixed w-full z-10 bg-secondary glass-header">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
