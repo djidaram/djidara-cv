84b752930e0f656b7aa851613b2d47abe0e704e2
mdjidara <m.djidara@pan-net.eu>
2024-07-30 19:27:17 +0200

    feat: send email api

---
+++ app/api/send/route.ts
@@ -0,0 +1,26 @@
+import { EmailTemplate } from "../../../components/EmailTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const toEmail = String(process.env.TO_EMAIL);
+
+export const POST = async (req: Request) => {
+  const { email, subject, message } = await req.json();
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "djidara-cv <resend@djidara.me>",
+      to: [toEmail],
+      subject: subject,
+      react: EmailTemplate({ email, message }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+};
